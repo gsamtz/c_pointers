3547c55405878a0abc15c42a76d6dfa92c1adff0
Sam <121076311+gsamtz@users.noreply.github.com>
2024-11-16 12:05:44 -0600

    Initial Commit

---
+++ c_pointers/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c_pointers
+//
+//  Created by Samuel Gutierrez on 11/16/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
