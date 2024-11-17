eee4bb0b0048ca34d8736af59ad6a0d6a02d6d1a
Sam <121076311+gsamtz@users.noreply.github.com>
2024-11-16 21:57:50 -0600

    Add intended behavior

--- c_pointers/multiple_indirection.c
+++ c_pointers/multiple_indirection.c
@@ -5,10 +5,70 @@
 //  Created by Samuel Gutierrez on 11/16/24.
 //
 
+#include <stdlib.h>
 #include "multiple_indirection.h"
 
 char *words[LENGTH];
 
 void multiple_indirection_char(void) {
-    printf("Hello world!\n");
+    printf("Multiple indirection char example\n");
+    
+    int i;
+    char *tempWord;
+    int strLen;
+    
+    for (i = 0; i < LENGTH; i++) {
+        tempWord = (char *) malloc(WORD_LENGTH);
+        
+        printf("Enter word (15 characters MAX): ");
+        fgets(tempWord, WORD_LENGTH, stdin);
+        
+        strLen = length(tempWord);
+        tempWord[strLen - 1] = '\0';
+        
+        updateWordArr(i, strLen, tempWord);
+        
+        strLen = 0;
+        free(tempWord);
+    }
+    // Display each string and free its allocated memory
+    displayWords();
+}
+
+void displayWords(void) {
+    char **wpp;
+    char *wp;
+    for (int i = 0; i < LENGTH; i++) {
+        wpp = words + i;
+        wp = *wpp;
+        printf("%c", *wp);
+        ++wp;
+        while (*wp != '\0') {
+            printf(" %c", *wp);
+            ++wp;
+        }
+        printf("\n\n");
+        free(words[i]);
+    }
+}
+
+void updateWordArr(int index, int strLength, const char *str) {
+    words[index] = (char *) malloc(strLength);
+    char *tempStr = words[index];
+    int i = 0;
+    while (i < strLength) {
+        tempStr[i] = *str;
+        ++str;
+        ++i;
+    }
+}
+
+int length(const char *str) {
+    int len = 0;
+    while (*str != 0) {
+        ++len;
+        ++str;
+    }
+    printf("length: %d\n\n", len);
+    return len;
 }
--- c_pointers/multiple_indirection.h
+++ c_pointers/multiple_indirection.h
@@ -11,7 +11,11 @@
 #include <stdio.h>
 
 #define LENGTH 3
+#define WORD_LENGTH 16
 
 void multiple_indirection_char(void);
+void updateWordArr(int index, int strLength, const char *str);
+void displayWords(void);
+int length(const char * str);
 
 #endif /* multiple_indirection_h */
